a65b5d6d62a1017af2d70e2520efb755d5da3fe8
Lucas Alves <lucasalves.souza01@gmail.com>
2021-07-14 10:57:22 -0300

    test: create intital setup for mongo test

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,13 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(String(process.env.MONGO_URL))
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('', () => {})
+})
