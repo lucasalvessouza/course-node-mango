e9034b8f5c3bb5ab56890355dc31345e2810de7d
Lucas Alves <lucasalves.souza01@gmail.com>
2021-07-21 20:12:44 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,49 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
+
+interface StubType {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return new Promise(resolve => resolve({
+        body: {},
+        statusCode: 200
+      }))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): StubType => {
+  const controllerStub = makeControllerStub()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogController decorator', () => {
+  test('should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+
+    const httpRequest = {
+      body: {
+        name: 'Lucas Alves',
+        email: 'lucas@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password_confirmation'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
