37beac383fbc1e2c6263659bf2dce13e99c358b0
Lucas Alves <lucasalves.souza01@gmail.com>
2021-08-12 08:39:40 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -145,4 +145,11 @@ describe('DbAuthentication Usacase', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  test('should call TokenGenerator with correct id', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+
+    expect(accessToken).toBe('any_token')
+  })
 })
